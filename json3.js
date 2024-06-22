1be008d64545df8240bc00e1df6d26c6740cf949
Manggla52 <Michaelmartinez427@gmail.com>
2024-06-22 16:25:59 -0500

    update

---
+++ json3.js
@@ -0,0 +1,5 @@
+function truckChanger (){
+    whtAboutTrucksHeader.innerHTML = "I like the Chevy Silverado!";
+    yourFavTruckPara.innerHTML = "I am a Heavy Chevy Man!!!";
+}
+mytruckSwpButtn.oneclick = truckChanger;
---
+++ truckChanger.js
@@ -0,0 +1,3 @@
+function truckChanger({
+    whtAboutTrucksHeader, innerHTML = "Chevy Silverado", yourFavTruckPara, innerHTML = "I'm just a Heavy Chevy Man!!!"
+});
